3028062493398c73a41fb88b2477d982f7920365
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-11-28 15:09:31 +0900

    추가 테스트케이스 작성

--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -14,4 +14,22 @@ public class SimpleCalculatorTest {
 
         assertEquals(3, rs);
     }
+
+    @Test
+    @DisplayName("2 + 10 = 12")
+    public void testPlus2() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(2, 10);
+
+        assertEquals(12, rs);
+    }
+
+    @Test
+    @DisplayName("10 + 20 = 30")
+    public void testPlus3() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(10, 20);
+
+        assertEquals(30, rs);
+    }
 }
